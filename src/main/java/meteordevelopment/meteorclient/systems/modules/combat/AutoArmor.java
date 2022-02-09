19178e6bb830cf78dac89d56185961cdfda5bf36
菾凴 <71136405+buiawpkgew1@users.noreply.github.com>
2022-02-09 10:32:24 +0800

    Update AutoArmor.java

--- src/main/java/meteordevelopment/meteorclient/systems/modules/combat/AutoArmor.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/combat/AutoArmor.java
@@ -83,7 +83,7 @@ public class AutoArmor extends Module {
     private int timer;
 
     public AutoArmor() {
-        super(Categories.Combat, "auto-armor", "Automatically equips armor.");
+        super(Categories.Combat, "自动装备", "自动装备盔甲.");
 
         armorPieces[0] = helmet;
         armorPieces[1] = chestplate;
